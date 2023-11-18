73915cf1d50fac439f17710ef8449ebc4b3a32db
Hiritik sonar <snarhrithik0977@gmail.com>
2023-11-18 11:12:12 +0545

    Second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,8 +8,12 @@ import heroImg03 from '../assets/images/c-med.jpg';
 import icon01 from '../assets/images/icon01.png';
 import icon02 from '../assets/images/icon02.png';
 import icon03 from '../assets/images/icon03.png';
+import featureImg from '../assets/images/feature-img.png';
+import videoIcon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png';
 import {Link} from 'react-router-dom';
 import {BsArrowRight } from 'react-icons/bs';
+import ServiceList from '../components/Services/ServiceList';
 
 const Home = () => {
   return (
@@ -154,6 +158,8 @@ const Home = () => {
         </div>
       </div>
      </section>
+
+
      <div className="flex ">
   <div className="container bg-irisBlueColor ml-[20px] p-4 rounded-md mr-4">
     <label className='text-[26px] leading-9 text-headingColor font-[700] text-center' htmlFor="">Find a doctor</label>
@@ -170,8 +176,75 @@ const Home = () => {
   
 </div>
 
-
     <About/>
+
+    {/*------------------SERVICES---------------------*/ }
+    <section>
+      <div className="container">
+        <div className='xl:w-[470px] mx-auto'>
+          <h2 className='heading text-center'>Our medical services</h2>
+          <p className='text__para text-center'>Trust in us we will never let you down. Our health expertise will give u best medical experience.</p>
+        </div>
+
+        <ServiceList/>
+      </div>
+    </section>
+
+
+
+    {/*------------------FEATURES---------------------*/ }
+
+    <section>
+      <div className="container">
+        <div className='flex items-center justify-between flex-col lg:flex-row'>
+
+          {/*-----------------feature content---------------------*/ }
+          <div className='xl:w-[670px]'>
+            <h2 className='heading'>Get virtual treatment <br/>anytime.</h2>
+            <ul className="pl-4">
+              <li className="text__para">
+                1. Schedule the appointment directly.
+              </li>
+              <li className="text__para">
+                2. Search the physician here and contact directly.
+              </li>
+              <li className="text__para">
+                3. View our physicians who are accepting new patients, use the online scheduling tool to select an appoitment time.
+              </li>
+            </ul>
+            <Link to='/' ><button className='btn'>Learn More</button></Link>
+          </div>
+
+
+             {/*-----------------feature image---------------------*/ }
+             <div className='relative z-10 xl:w-[470px] flex justify-end mt-[50px] lg:mt-0'>
+              <img src={featureImg} className='w-3/4' alt="" />
+              <div className='w-[150px] h-[145px] lg:w-[200px] bg-white absolute bottom-[100px] left-0 md:bottom-[30px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
+
+                <div className="flex items-center justify between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>Tue, 24</p>
+                    <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400] '>10:00</p>
+
+                  </div>
+                  <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px] ml-6'>
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 textirisBlueColor font-[500] mt-2 lg:mt-4 rounded-full'>Consulation</div>
+
+                <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                  <img src={avatarIcon} alt="" />
+                </div>
+
+              </div>
+             </div>
+
+        </div>
+      </div>
+    </section>
+   
 
   </>
 
